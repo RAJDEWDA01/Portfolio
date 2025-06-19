34bbcdb30f5d6987d20e557c37ac19ccda04a327
raj dewda <rajdewda@outlook.com>
2025-06-19 17:54:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+// ScrollReveal animations
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+// // Typed.js text animation
+// const typed = new Typed('.multiple-text', {
+//     strings: ['Raj Dangi', 'S/O Mr. Satish Dangi', 'Web Developer'],
+//     typeSpeed: 60,
+//     backSpeed: 40,
+//     backDelay: 2000,
+//     loop: true
+// });
